e90316819a83769bd6108a99c97aa92ab077c505
AnthonyViscusi <anthony.viscusi@gmail.com>
2026-03-07 22:38:24 -0500

    implemented move and groups

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
+
     player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -20,8 +24,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player1.draw(screen)
-        player1.update(dt)
+        updatable.update(dt)
+        for o in drawable:
+            o.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
