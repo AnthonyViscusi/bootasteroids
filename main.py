0223b785bc3c72e7e6adb96029c4ded585158ea3
AnthonyViscusi <anthony.viscusi@gmail.com>
2026-03-07 22:04:00 -0500

    enabled player rotation

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    
     player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -21,6 +21,7 @@ def main():
                 return
         screen.fill("black")
         player1.draw(screen)
+        player1.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
